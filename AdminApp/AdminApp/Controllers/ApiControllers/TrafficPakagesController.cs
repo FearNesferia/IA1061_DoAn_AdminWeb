aa229b816df42316c00de1a7d208b958dda59508
TIENHBSE61862\FearNesferia <fearnesferia@gmail.com>
2017-11-21 21:47:59 +0700

    xong cai luong chuong trinh goy
    xong cai luong chuong trinh goy

--- AdminApp/AdminApp/Controllers/ApiControllers/TrafficPakagesController.cs
+++ AdminApp/AdminApp/Controllers/ApiControllers/TrafficPakagesController.cs
@@ -17,23 +17,32 @@ namespace AdminApp.Controllers.ApiControllers
     public class TrafficPakagesController : ApiController
     {
         private PakageDBContext context = new PakageDBContext();
+
+        [HttpGet]
+        [Route("v1/api/TrafficPakages")]
+        public IHttpActionResult Get()
+        {
+            return Json(new { test = true });
+        }
+
         [HttpPost]
         [Route("v1/api/TrafficPakages")]
         public IHttpActionResult Post([FromBody]TrafficPackageModel request)
         {
 
             TrafficPackage package = new TrafficPackage(request.Path, request.QueryString, request.Payload);
-            TrafficPackage tp = this.context.TrafficPackages.FirstOrDefault(x => x.Id == package.Id);
+            //TrafficPackage tp = this.context.TrafficPackages.FirstOrDefault(x => x.Id == package.Id);
 
-            if (tp != null)
-            {
-                return Json(new { isAttack = tp.IsAttack });
-            }
-            this.context.TrafficPackages.Add(package);
+            //if (tp != null)
+            //{
+            //    return Json(new { isAttack = tp.IsAttack });
+            //}
+            //this.context.TrafficPackages.Add(package);
 
             // Call R here
             AnalyzePacket rHandler = new AnalyzePacket();
-            package.IsAttack = rHandler.GetAnalyzePacketResult(new int[] { package.LengthOfArguments, package.NumberOfArguments, package.NumberOfDigitsInArguments, package.NumberOfOtherCharInArguments, package.NumberOfDigitsInPath, package.NumberOfSpecialCharInArguments, package.LengthOfPath, package.LengthOfRequest, package.NumberOfLettersInArguments, package.NumberOfLettersCharInPath, package.NumberOfSepicalCharInPath });
+            package.IsAttack = rHandler.GetAnalyzePacketResult(new int[] { package.LengthOfArguments, package.NumberOfArguments, package.NumberOfDigitsInArguments, package.NumberOfOtherCharInArguments, package.NumberOfDigitsInPath, package.NumberOfSpecialCharInArguments, package.LengthOfPath, package.LengthOfRequest, package.NumberOfLettersInArguments, package.NumberOfLettersCharInPath, package.NumberOfSepicalCharInPath, 0 });
+            rHandler.DisposeConnection();
 
             return Json(new { isAttack = package.IsAttack });
         }
